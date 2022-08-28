077aabf226c886db93c8ac64744145963b4a5036
supplepentan <pentagramog@gmail.com>
2022-08-28 14:45:43 +0900

    update

--- src/components/CanvasWebCamera.vue
+++ src/components/CanvasWebCamera.vue
@@ -126,10 +126,10 @@ const stopStreamedVideo = (video) => {
 </script>
 <template>
   <div>
-    <div id="containerCanvas" class="grid grid-cols-2 gap-4 py-2">
-      <div>
+    <div id="containerCanvas" class="grid grid-cols-2 py-2">
+      <div class="">
         <h1 class="text-center">Real Time</h1>
-        <div class="relative flex justify-center py-2">
+        <div class="relative py-2 border-2 border-sky-200 flex justify-center">
           <canvas v-bind:width="canvasWidth" v-bind:height="canvasHeight" class="absolute classCanvas"
             id="canvasVideo"></canvas>
           <canvas v-bind:width="canvasWidth" v-bind:height="canvasHeight" class="absolute classCanvas"
@@ -140,7 +140,7 @@ const stopStreamedVideo = (video) => {
       </div>
       <div>
         <h1 class="text-center">Capture</h1>
-        <div class="flex justify-center py-2">
+        <div class="py-2 border-2 border-sky-200 flex justify-center">
           <canvas v-bind:width="canvasWidth" v-bind:height="canvasHeight" class="classCanvas"
             id="canvasConcat"></canvas>
         </div>
@@ -148,36 +148,22 @@ const stopStreamedVideo = (video) => {
     </div>
   </div>
   <div class="grid grid-cols-2 border-2">
-    <div class="p-2 border-2 rounded border-sky-400">
+    <div class="p-2 border-2 rounded border-sky-200">
       <p class="text-center">Photoframe</p>
       <input type="file" v-on:change="fileSelected">
     </div>
-    <div class="p-2 border-2 rounded border-sky-400">
+    <div class="p-2 border-2 rounded border-sky-200">
       <p class="text-center border">Stamp</p>
       <div>
-        <img src="../../assets/img/supplepentan-favicon.png" alt="" v-on:click="onStamp">
+        <img src="/supplepentan-favicon.png" alt="" v-on:click="onStamp">
       </div>
     </div>
   </div>
   <div class="flex items-stretch">
     <button type="button" class="flex-1 classButton" v-on:click="concatImage">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera"
-        viewBox="0 0 16 16">
-        <path
-          d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z" />
-        <path
-          d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
-      </svg>
       <p>Capture</p>
     </button>
     <button type="button" class="flex-1 classButton" v-on:click="download">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-arrow-down"
-        viewBox="0 0 16 16">
-        <path fill-rule="evenodd"
-          d="M7.646 10.854a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 9.293V5.5a.5.5 0 0 0-1 0v3.793L6.354 8.146a.5.5 0 1 0-.708.708l2 2z" />
-        <path
-          d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
-      </svg>
       <p>Download</p>
     </button>
     <button type="button" class="flex-1 classButton" v-on:click="frameStop">
@@ -193,10 +179,10 @@ const stopStreamedVideo = (video) => {
 </template>
 <style scoped>
 .classCanvas {
-  @apply rounded ring-4
+  @apply rounded
 }
 
 .classButton {
-  @apply block p-2 rounded bg-neutral-200 ring-2;
+  @apply block p-2 rounded bg-neutral-200 border-2 border-sky-200;
 }
 </style>
